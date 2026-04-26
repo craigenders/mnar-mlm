68aa103beb85b2b0d64d0fb87df881acf911867e
craigenders <cenders@ucla.edu>
2026-04-26 09:32:20 -0700

    Update 0. Run all Analyses.R

--- 0. Run all Analyses.R
+++ 0. Run all Analyses.R
@@ -6,83 +6,83 @@ library(rblimp)
 
 # set_blimp('/applications/blimp/blimp')
 
-# source('1. Analysis Growth Intermittent.R')
-
-# write.csv(tab_growth_im,file = '~/desktop/MNAR Results/tab_growth_im.csv')
-# write.csv(es_growth_im,file = '~/desktop/MNAR Results/es_growth_im.csv')
-# write.csv(conv_growth_im,file = '~/desktop/MNAR Results/conv_growth_im.csv')
-
-# source('2. Analysis Growth Dropout.R')
+# source('1. Analysis Growth Dropout.R')
 
 # write.csv(tab_growth_do,file = '~/desktop/MNAR Results/tab_growth_do.csv')
 # write.csv(es_growth_do,file = '~/desktop/MNAR Results/es_growth_do.csv')
 # write.csv(conv_growth_do,file = '~/desktop/MNAR Results/conv_growth_do.csv')
 
-# source('3. Analysis Intensive Intermittent.R')
+# source('2. Analysis Growth Intermittent.R')
 
-# write.csv(tab_intensive_im,file = '~/desktop/MNAR Results/tab_intensive_im.csv')
-# write.csv(es_intensive_im,file = '~/desktop/MNAR Results/es_intensive_im.csv')
-# write.csv(conv_intensive_im,file = '~/desktop/MNAR Results/conv_intensive_im.csv')
+# write.csv(tab_growth_im,file = '~/desktop/MNAR Results/tab_growth_im.csv')
+# write.csv(es_growth_im,file = '~/desktop/MNAR Results/es_growth_im.csv')
+# write.csv(conv_growth_im,file = '~/desktop/MNAR Results/conv_growth_im.csv')
 
-# source('4. Analysis Intensive Dropout.R')
+# source('3. Analysis Intensive Dropout.R')
 
 # write.csv(tab_intensive_do,file = '~/desktop/MNAR Results/tab_intensive_do.csv')
 # write.csv(es_intensive_do,file = '~/desktop/MNAR Results/es_intensive_do.csv')
 # write.csv(conv_intensive_do,file = '~/desktop/MNAR Results/conv_intensive_do.csv')
 
+# source('4. Analysis Intensive Intermittent.R')
+
+# write.csv(tab_intensive_im,file = '~/desktop/MNAR Results/tab_intensive_im.csv')
+# write.csv(es_intensive_im,file = '~/desktop/MNAR Results/es_intensive_im.csv')
+# write.csv(conv_intensive_im,file = '~/desktop/MNAR Results/conv_intensive_im.csv')
+
 # RUN NIGHTLY ----
 
 set_blimp('/applications/blimp/blimp-nightly')
 
-source('1. Analysis Growth Intermittent.R')
-
-write.csv(tab_growth_im,file = '~/desktop/MNAR Results/tab_growth_im_ni.csv')
-write.csv(es_growth_im,file = '~/desktop/MNAR Results/es_growth_im_ni.csv')
-write.csv(conv_growth_im,file = '~/desktop/MNAR Results/conv_growth_im_ni.csv')
-
-source('2. Analysis Growth Dropout.R')
+source('1. Analysis Growth Dropout.R')
 
 write.csv(tab_growth_do,file = '~/desktop/MNAR Results/tab_growth_do_ni.csv')
 write.csv(es_growth_do,file = '~/desktop/MNAR Results/es_growth_do_ni.csv')
 write.csv(conv_growth_do,file = '~/desktop/MNAR Results/conv_growth_do_ni.csv')
 
-source('3. Analysis Intensive Intermittent.R')
+source('2. Analysis Growth Intermittent.R')
 
-write.csv(tab_intensive_im,file = '~/desktop/MNAR Results/tab_intensive_im_ni.csv')
-write.csv(es_intensive_im,file = '~/desktop/MNAR Results/es_intensive_im_ni.csv')
-write.csv(conv_intensive_im,file = '~/desktop/MNAR Results/conv_intensive_im_ni.csv')
+write.csv(tab_growth_im,file = '~/desktop/MNAR Results/tab_growth_im_ni.csv')
+write.csv(es_growth_im,file = '~/desktop/MNAR Results/es_growth_im_ni.csv')
+write.csv(conv_growth_im,file = '~/desktop/MNAR Results/conv_growth_im_ni.csv')
 
-source('4. Analysis Intensive Dropout.R')
+source('3. Analysis Intensive Dropout.R')
 
 write.csv(tab_intensive_do,file = '~/desktop/MNAR Results/tab_intensive_do_ni.csv')
 write.csv(es_intensive_do,file = '~/desktop/MNAR Results/es_intensive_do_ni.csv')
 write.csv(conv_intensive_do,file = '~/desktop/MNAR Results/conv_intensive_do_ni.csv')
 
+source('4. Analysis Intensive Intermittent.R')
+
+write.csv(tab_intensive_im,file = '~/desktop/MNAR Results/tab_intensive_im_ni.csv')
+write.csv(es_intensive_im,file = '~/desktop/MNAR Results/es_intensive_im_ni.csv')
+write.csv(conv_intensive_im,file = '~/desktop/MNAR Results/conv_intensive_im_ni.csv')
+
 # RUN DROPOUT COMMAND ----
 
 set_blimp('/applications/blimp/blimp-nightly')
 
-source('1. Analysis Growth Intermittent FIN.R')
-
-write.csv(tab_growth_im,file = '~/desktop/MNAR Results/tab_growth_im_fi.csv')
-write.csv(es_growth_im,file = '~/desktop/MNAR Results/es_growth_im_fi.csv')
-write.csv(conv_growth_im,file = '~/desktop/MNAR Results/conv_growth_im_fi.csv')
-
-source('2. Analysis Growth Dropout FIN.R')
+source('1. Analysis Growth Dropout FIN.R')
 
 write.csv(tab_growth_do,file = '~/desktop/MNAR Results/tab_growth_do_fi.csv')
 write.csv(es_growth_do,file = '~/desktop/MNAR Results/es_growth_do_fi.csv')
 write.csv(conv_growth_do,file = '~/desktop/MNAR Results/conv_growth_do_fi.csv')
 
-source('3. Analysis Intensive Intermittent FIN.R')
+source('2. Analysis Growth Intermittent FIN.R')
 
-write.csv(tab_intensive_im,file = '~/desktop/MNAR Results/tab_intensive_im_fi.csv')
-write.csv(es_intensive_im,file = '~/desktop/MNAR Results/es_intensive_im_fi.csv')
-write.csv(conv_intensive_im,file = '~/desktop/MNAR Results/conv_intensive_im_fi.csv')
+write.csv(tab_growth_im,file = '~/desktop/MNAR Results/tab_growth_im_fi.csv')
+write.csv(es_growth_im,file = '~/desktop/MNAR Results/es_growth_im_fi.csv')
+write.csv(conv_growth_im,file = '~/desktop/MNAR Results/conv_growth_im_fi.csv')
 
-source('4. Analysis Intensive Dropout FIN.R')
+source('3. Analysis Intensive Dropout FIN.R')
 
 write.csv(tab_intensive_do,file = '~/desktop/MNAR Results/tab_intensive_do_fi.csv')
 write.csv(es_intensive_do,file = '~/desktop/MNAR Results/es_intensive_do_fi.csv')
 write.csv(conv_intensive_do,file = '~/desktop/MNAR Results/conv_intensive_do_fi.csv')
+
+source('4. Analysis Intensive Intermittent FIN.R')
+
+write.csv(tab_intensive_im,file = '~/desktop/MNAR Results/tab_intensive_im_fi.csv')
+write.csv(es_intensive_im,file = '~/desktop/MNAR Results/es_intensive_im_fi.csv')
+write.csv(conv_intensive_im,file = '~/desktop/MNAR Results/conv_intensive_im_fi.csv')
 
